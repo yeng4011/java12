4dc3dca901ffc5916ffaee7e4da0253de5820cdd
yeng4011 <89770222+yeng4011@users.noreply.github.com>
2022-01-03 02:28:23 +0800

    Create Class14.java

---
+++ Class14.java
@@ -0,0 +1,12 @@
+package Class14.java;
+
+ public class Class14
+ {
+ public static void main(String args[])
+ {
+
+ String str="Habit is second nature.";
+ System.out.println("string="+str);
+ System.out.println("sub-string="+str.substring(16,22));
+ }
+ }
