53829135175f71badb4bfd835c9f1bf4b28c05a9
yeng4011 <89770222+yeng4011@users.noreply.github.com>
2022-01-03 02:29:32 +0800

    Create Class16.java

---
+++ Class16.java
@@ -0,0 +1,12 @@
+package Class16.java;
+
+ public class Class16
+ {
+ public static void main(String args[])
+ {
+ String str="262904713"; 
+ int num;
+ num=Integer.parseInt(str);
+ System.out.println(num+"*2="+num*2);
+ }
+ } 
