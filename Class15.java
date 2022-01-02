7a9871f167e4635fd502dc7a52c381a70904f85f
yeng4011 <89770222+yeng4011@users.noreply.github.com>
2022-01-03 02:29:05 +0800

    Create Class15.java

---
+++ Class15.java
@@ -0,0 +1,13 @@
+package Class15.java;
+
+
+ public class Class15
+ {
+ public static void main(String args[])
+ {
+ String str;
+ str=Float.toString(123.45f);
+ System.out.println(str);
+ System.out.println("length="+str.length());
+ }
+ }
